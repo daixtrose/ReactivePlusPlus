c12474a6a1f79a0d5d15c1b18e74bc9b75df79d7
Aleksey Loginov <victimsnino@gmail.com>
2024-05-13 21:58:19 +0300

    add grpc to presets

--- conanfile.py
+++ conanfile.py
@@ -6,6 +6,5 @@ class Config(ConanFile):
 
     def requirements(self):
         self.requires("sfml/2.6.1")
-        # self.requires("grpc/1.54.3", transitive_libs=True, transitive_headers=True)
-        # self.requires("protobuf/3.21.12")
-        # self.requires("libmount/2.39", override=True)
+        self.requires("grpc/1.54.3", transitive_libs=True, transitive_headers=True)
+        self.requires("protobuf/3.21.12")
