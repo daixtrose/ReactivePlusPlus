90250bc6c4284bd5559e8b729a455dc279229dc8
Aleksey Loginov <victimsnino@gmail.com>
2024-05-14 11:18:56 +0300

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 
 class Config(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps"
+    generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
         self.requires("sfml/2.6.1")
