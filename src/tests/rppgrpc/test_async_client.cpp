d98f8e9251db0e97b7b2d5e56febcb5a978ca8ca
Aleksey Loginov <victimsnino@gmail.com>
2024-05-19 18:15:21 +0300

    try to fix

--- src/tests/rppgrpc/test_async_client.cpp
+++ src/tests/rppgrpc/test_async_client.cpp
@@ -46,12 +46,9 @@ TEST_CASE("async client can be casted to rppgrpc")
             {
                 Input message{};
                 message.set_value(v);
-                std::cout << message.ShortDebugString() << std::endl;
 
                 subj.get_observer().on_next(message);
-                fakeit::Verify(OverloadedMethod(stream_mock, Write, void(const Input* req, grpc::WriteOptions)).Matching([message](const Input* req, grpc::WriteOptions) -> bool {
-                    std::cout << req->ShortDebugString() << " - " << message.ShortDebugString() << std::endl;
-                    return req->value() == message.value(); })).Once();
+                fakeit::Verify(OverloadedMethod(stream_mock, Write, void(const Input* req, grpc::WriteOptions)).Matching([v](const Input* req, grpc::WriteOptions) -> bool { return req->value() == v; })).Once();
                 reactor->OnWriteDone(true);
             }
         }
